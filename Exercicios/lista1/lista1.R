8f095dd13db04cc16b80d30c4cb3a90333c8c75c
Gabriel Tormin Alves <gta1998@gmail.com>
2020-03-10 19:49:01 -0300

    atalização de teste

---
+++ Exercicios/lista1/lista1.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+########exercicio 1###########
+amostra <- function(n,lambda=1,gamma=1){
+  (log(runif(n)/lambda)/-lambda)+gamma
+}
+
+amostra(10)
+
+##########exercicio 2#########
+
+variavel <- 'teste'
